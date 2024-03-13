0cb491c3cf4e5a968fe63e165de2ba99e3cb6a5e
Gowtham Dharma <gowthamdharma.e@codingmart.com>
2024-03-13 12:30:55 +0530

    Added getUpcomingDrive function

--- src/api/admin/drive/drive.route.ts
+++ src/api/admin/drive/drive.route.ts
@@ -2,62 +2,78 @@ import { FastifyInstance } from "fastify";
 import { API_METHODS } from "../../../interface/api.interface";
 import { IRouteOptions } from "../../../interface/fastify.interface";
 import { jwtAdminVerification } from "../../../preHandlers/preAdminAuth";
-import { addDrive, getDetails, addFeedback, removeFeedBack, deleteDrive, getCampusYears, startDrive, stopDrive, uploadDatabase } from "./drive.controller"
-
+import {
+  addDrive,
+  getDetails,
+  addFeedback,
+  removeFeedBack,
+  deleteDrive,
+  getCampusYears,
+  startDrive,
+  stopDrive,
+  uploadDatabase,
+  getUpcomingDrive,
+} from "./drive.controller";
 
 const driveRoutes: IRouteOptions<{
-    Params: any;
-    Body: any;
-    Querystring: any;
+  Params: any;
+  Body: any;
+  Querystring: any;
 }>[] = [
-        {
-            url: '/addDrive',
-            handler: addDrive,
-            preHandler: [jwtAdminVerification],
-            method: API_METHODS.POST,
-        },
-        {
-            url: '/details',
-            handler: getDetails,
-            preHandler: [jwtAdminVerification],
-            method: API_METHODS.GET,
-        },
-        {
-            url: '/feedback',
-            handler: addFeedback,
-            preHandler: [jwtAdminVerification],
-            method: API_METHODS.POST,
-        },
-        {
-            url: '/feedback/:id',
-            handler: removeFeedBack,
-            preHandler: [jwtAdminVerification],
-            method: API_METHODS.PUT,
-        },
-        {
-            url: '/drive/:id',
-            handler: deleteDrive,
-            preHandler: [jwtAdminVerification],
-            method: API_METHODS.PUT,
-        },
-        {
-            url: '/campusYears',
-            handler: getCampusYears,
-            preHandler: [jwtAdminVerification],
-            method: API_METHODS.GET,
-        },
-        {
-            url: '/startDrive',
-            handler: startDrive,
-            preHandler: [jwtAdminVerification],
-            method: API_METHODS.POST,
-        },
-        {
-            url: '/stopDrive',
-            handler: stopDrive,
-            preHandler: [jwtAdminVerification],
-            method: API_METHODS.PUT,
-        }
-    ];
+  {
+    url: "/addDrive",
+    handler: addDrive,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.POST,
+  },
+  {
+    url: "/details",
+    handler: getDetails,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.GET,
+  },
+  {
+    url: "/upcomingDrive",
+    handler: getUpcomingDrive,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.GET,
+  },
+  {
+    url: "/feedback",
+    handler: addFeedback,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.POST,
+  },
+  {
+    url: "/feedback/:id",
+    handler: removeFeedBack,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.PUT,
+  },
+  {
+    url: "/drive/:id",
+    handler: deleteDrive,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.PUT,
+  },
+  {
+    url: "/campusYears",
+    handler: getCampusYears,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.GET,
+  },
+  {
+    url: "/startDrive",
+    handler: startDrive,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.POST,
+  },
+  {
+    url: "/stopDrive",
+    handler: stopDrive,
+    preHandler: [jwtAdminVerification],
+    method: API_METHODS.PUT,
+  },
+];
 
 export default driveRoutes;
