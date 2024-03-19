51f7fcfb4f9cf128c62eadeac7092fcd3b640b02
Gowtham Dharma <gowthamdharma.e@codingmart.com>
2024-03-19 17:25:27 +0530

    refactored results

--- src/api/admin/tabSwitch/index.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from "fastify";
-import tabSwitchRoutes from "./tabSwitch.route";
-
-export default async (fastify: FastifyInstance) => {
-  for (const tabSwitchRoute of tabSwitchRoutes) {
-    fastify.route(tabSwitchRoute);
-  }
-};
